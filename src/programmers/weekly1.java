351aa15a9eb4c15268288f7899ab28801d3caebb
hjhng125 <hjhng125@nate.com>
2021-08-09 22:02:04 +0900

    Programmers Weekly1

---
+++ src/programmers/weekly1.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class weekly1 {
+    public static void main(String[] args) {
+
+        long result = new Solution().solution(3, 20, 4);
+        System.out.println(result);
+
+    }
+
+    static class Solution {
+
+        public long solution(int price, int money, int count) {
+            long answer = price * ((long) count * (count + 1) / 2) - money;
+            return answer> 0 ? answer : 0;
+        }
+    }
+}
